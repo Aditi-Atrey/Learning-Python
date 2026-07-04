9c3ea2e3651fb6576da9aa220daeeb2e433693b2
Aditi Atrey <aditiatreymgs@gmail.com>
2026-07-04 15:29:23 +0530

    Refactor variable creation and print statements

--- Variable, print(), data type, type(), isinstance().py
+++ Variable, print(), data type, type(), isinstance().py
@@ -1,59 +1,59 @@
 #Creating variables and using print function
 print("Hello World!")
-name="Aditi"
-surname="Atrey"
-age="17"
+name = "Aditi"
+surname = "Atrey"
+age = "17"
 print("Name:",name,surname,"Age:",age)
 
 #Common Data Types
-int_var=10
+int_var = 10
 print("Interger:",int_var)
-float_var=6.7
+float_var = 6.7
 print("Float:",float_var)
-str_var="blue"
+str_var = "blue"
 print("String:",str_var)
-boolean_var=True
+boolean_var = True
 print("Boolean:",boolean_var)
-set_var={0.5,4,"apple"}
+set_var = {0.5,4,"apple"}
 print("Set:",set_var)
-dictionary_var={"Name":"Aditi","Age":17}
+dictionary_var = {"Name":"Aditi","Age":17}
 print("Dictionary:",dictionary_var)
-tuple_var=(7,"hello",8.5,7)
+tuple_var = (7,"hello",8.5,7)
 print("Tuple:",tuple_var)
-range_var=range(6)
+range_var = range(6)
 print("Range:",range_var)
-list_var=[1.2,2,"Mango",True,2]
+list_var = [1.2,2,"Mango",True,2]
 print("List:",list_var)
-none_var=None
+none_var = None
 print("None:",none_var)
 
 #Using type() 
-x="Aditi"
+x = "Aditi"
 print(type(x))
-y=7
+y = 7
 print(type(y))
-z=-5.5
+z = -5.5
 print(type(z))
-a=True
+a = True
 print(type(a))
-b={0.5,4,"apple"}
+b = {0.5,4,"apple"}
 print(type(b))
-c={"Name":"Aditi","Country":"India"}
+c = {"Name":"Aditi","Country":"India"}
 print(type(c))
-d=(7.8,4,"orange")
+d = (7.8,4,"orange")
 print(type(d))
-e=range(8)
+e = range(8)
 print(type(e))
-f=[1.8,2,9,True,"Yes"]
+f = [1.8,2,9,True,"Yes"]
 print(type(f))
-g=None
+g = None
 print(type(g))
 
 #Using isinstance()
-p="12"
+p = "12"
 print("Is it an integer:",isinstance(p,int))
-q=12
+q = 12
 print("Is it an integer or a float:",isinstance(q,(int,float)))
-r=1.6
+r = 1.6
 print("Is it an integer or/and a float:",isinstance(r,(int,float)))
 
