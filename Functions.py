7cc118e883bac5aaa24eb67d4803a8af7f88b03c
Aditi Atrey <aditiatreymgs@gmail.com>
2026-07-05 17:29:01 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,23 @@
+#Functions
+
+input("Enter your name: ")
+input("Enter your age: ")
+
+def hello():
+    print("Hello World!")
+hello()
+
+def calculate_sum(a,b):
+    print(a + b)
+calculate_sum(3,1)
+
+my_sum = calculate_sum(3,1)
+print(my_sum)
+
+def calculate_difference(a,b):
+    return a - b
+my_difference = calculate_difference(3,1)
+print(my_difference)
+
+
+    
