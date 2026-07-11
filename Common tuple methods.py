2604b88967337b6bec2b8ba14fd5a6684eec1dd3
Aditi Atrey <aditiatreymgs@gmail.com>
2026-07-11 13:48:04 +0530

    Add files via upload

---
+++ Common tuple methods.py
@@ -0,0 +1,8 @@
+#Common Tuples Method
+
+fruits = ("plum", "apple", "grapes", "plum", "banana")
+print(fruits.count("plum"))
+print(fruits.index("apple"))
+print(fruits.index("plum", 1, 4))
+print(sorted(fruits))
+
