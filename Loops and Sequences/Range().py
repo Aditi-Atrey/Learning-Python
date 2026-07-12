f42d815919f6e43aebcb9700bc343808d63d9d76
Aditi Atrey <aditiatreymgs@gmail.com>
2026-07-12 17:42:54 +0530

    Add files via upload

---
+++ Loops and Sequences/Range().py
@@ -0,0 +1,16 @@
+#Range()
+
+for num in range(2,11,2):
+    print(num)
+
+for num in range(6):
+    print(num)
+
+for num in range(40, 30, -2):
+    print(num)
+
+for num in range(-6,2):
+    print(num)
+
+numbers = list(range(2,11,2))
+print(numbers)
