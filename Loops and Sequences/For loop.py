bf9df2903785db9d0ad2ee2bec45482e55167852
Aditi Atrey <aditiatreymgs@gmail.com>
2026-07-12 17:22:26 +0530

    Add files via upload

---
+++ Loops and Sequences/For loop.py
@@ -0,0 +1,17 @@
+#For loop
+
+programming_languages = ["Rust", "Python", "C++", "Java"]
+for language in programming_languages:
+    print(language)
+
+
+for char in "code":
+    print(char)
+
+#Nested for loop
+
+categories = ["Fruit", "Vegetable"]
+foods = ["Apple", "Carrot", "Banana"]
+for category in categories:
+    for food in foods:
+        print(category, food)
