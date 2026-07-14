a14e18cb9c720db2b625f09a4f073571af1c00ca
Aditi Atrey <aditiatreymgs@gmail.com>
2026-07-14 19:00:50 +0530

    Add files via upload

---
+++ Enumerate().py
@@ -0,0 +1,25 @@
+#Enumerate()
+
+languages = ["Spanish", "English", "Russian", "Chinese"]
+
+#Method 1
+
+index = 0
+
+for language in languages:
+    print(f"Index {index} and Language {language}")
+    index += 1
+
+#Method 2
+
+print(list(enumerate(languages)))
+
+#Method 3
+
+for index, language in enumerate(languages):
+    print(f"Index {index} and Language {language}")
+
+#Method 4 with index change
+
+for index, language in enumerate(languages, 1):
+    print(f"Index {index} and Language {language}")
