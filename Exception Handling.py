a8871f407ac1cf0515887866b3643016ac306e53
Aditi Atrey <aditiatreymgs@gmail.com>
2026-08-22 17:44:53 +0530

    Add files via upload

---
+++ Exception Handling.py
@@ -0,0 +1,13 @@
+try:
+    x = 10/0
+except ZeroDivisionError:
+    print("You can't divide number by 0!")
+
+try:
+    x = 10/2
+except ZeroDivisionError:
+    print("You can't divide by 0!")
+else:
+    print("Division successful!", x)
+finally:
+    print("This code always runs!")
