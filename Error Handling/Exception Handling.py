d18b9a6929442b8434d9dac53a47367434c2e1e0
Aditi Atrey <aditiatreymgs@gmail.com>
2026-08-22 17:51:23 +0530

    Add additional error handling for invalid input

--- Error Handling/Exception Handling.py
+++ Error Handling/Exception Handling.py
@@ -11,3 +11,11 @@ else:
     print("Division successful!", x)
 finally:
     print("This code always runs!")
+
+try:
+    number = int("abc")
+    x = 10 / number
+except ValueError:
+    print("This is an invalid number.")
+except ZeroDivisionError:
+    print("You can't divide by 0!")
