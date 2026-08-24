1637814a8d78e444e8c86743321cbfffee0bb215
Aditi Atrey <aditiatreymgs@gmail.com>
2026-08-24 16:19:25 +0530

    Add example for parsing configuration with error handling

--- Error Handling/Raise Statement.py
+++ Error Handling/Raise Statement.py
@@ -35,3 +35,15 @@ try:
     new_balance = withdraw(100,150)
 except InsufficientFundsError as e:
     print(f"Transaction failed: {e}")
+
+#Example 4
+def parse_config(filename):
+    try:
+        with open(filename, "r") as file:
+            data = file.read()
+            return int(data)
+    except FileNotFoundError:
+        raise ValueError("Configuration file is missing") from None
+    except ValueError as e:
+        raise ValueError("Invalid configuration format") from e
+config = parse_config("config.txt")
