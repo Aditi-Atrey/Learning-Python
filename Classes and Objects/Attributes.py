6b0c3e81c090a04a7d3d651b385ddcb21a9371d7
Aditi Atrey <aditiatreymgs@gmail.com>
2026-08-25 16:10:48 +0530

    Add Car class example with attributes
    Added a second example demonstrating a Car class with attributes.

--- Classes and Objects/Attributes.py
+++ Classes and Objects/Attributes.py
@@ -15,4 +15,19 @@ dog2 = Dog("Tom")
 print(dog2.species)
 print(dog2.name)
 
+#Example 2
+class Car:
+    def __init__(self, colour, model):
+        self.colour = colour
+        self.model = model
+
+car1 = Car("Yellow", "Mercedes G Wagon")
+car2 = Car("Violet", "Lamborghini Aventador")
+
+print(car1.colour)
+print(car2.colour)
+
+print(car1.model)
+print(car2.model)
+
     
