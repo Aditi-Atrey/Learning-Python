529001abf201f6c55a9aad01ed03d304ca253ed3
Aditi Atrey <aditiatreymgs@gmail.com>
2026-08-25 15:56:37 +0530

    Add files via upload

---
+++ Attributes.py
@@ -0,0 +1,18 @@
+#Example 1
+class Dog:
+    species = "French Bulldog" #Class Attribute
+
+    def __init__(self, name):
+        self.name = name #Instance Attribute
+
+print(Dog.species)
+
+dog1 = Dog("Jack")
+print(dog1.species)
+print(dog1.name)
+
+dog2 = Dog("Tom")
+print(dog2.species)
+print(dog2.name)
+
+    
