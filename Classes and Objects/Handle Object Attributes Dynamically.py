2f423801960c18d0c8448c84b34c84bfc5bfb382
Aditi Atrey <aditiatreymgs@gmail.com>
2026-09-02 16:20:23 +0530

    Enhance dynamic attribute handling in Person class
    Added functionality to loop through and display non-callable attributes of the Person object.

--- Classes and Objects/Handle Object Attributes Dynamically.py
+++ Classes and Objects/Handle Object Attributes Dynamically.py
@@ -12,3 +12,10 @@ print(getattr(person, "city", "Milano"))
 
 attr_name = input("Enter the attribute you want to see: ")
 print(getattr(person, attr_name, "Attribute not found"))
+
+# Loop through all attributes of the person object with dir() function
+for attr in dir(person):
+    # Ignore dunder methods like __init__ or __str__ and regular methods
+    if not attr.startswith("__") and not callable(getattr(person, attr)):
+        value = getattr(person, attr)
+        print(f"{attr}: {value}")
